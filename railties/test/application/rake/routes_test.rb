1d0cad016ae00ecb2a2349ca8b5a85521847e84d
Bob Farrell <git@bob.frl>
2020-05-28 15:04:29 +0100

    Refactor Amazon ingress logic to separate controllers
    Fix misc. routes-related Railties tests.

--- railties/test/application/rake/routes_test.rb
+++ railties/test/application/rake/routes_test.rb
@@ -23,6 +23,7 @@ module ApplicationTests
                    rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                           action_mailbox/ingresses/postmark/inbound_emails#create
                       rails_relay_inbound_emails POST   /rails/action_mailbox/relay/inbound_emails(.:format)                                              action_mailbox/ingresses/relay/inbound_emails#create
                    rails_sendgrid_inbound_emails POST   /rails/action_mailbox/sendgrid/inbound_emails(.:format)                                           action_mailbox/ingresses/sendgrid/inbound_emails#create
+                      rails_amazon_confirmations POST   /rails/action_mailbox/amazon/inbound_emails(.:format)                                             action_mailbox/ingresses/amazon/confirmations#create
                      rails_amazon_inbound_emails POST   /rails/action_mailbox/amazon/inbound_emails(.:format)                                             action_mailbox/ingresses/amazon/inbound_emails#create
              rails_mandrill_inbound_health_check GET    /rails/action_mailbox/mandrill/inbound_emails(.:format)                                           action_mailbox/ingresses/mandrill/inbound_emails#health_check
                    rails_mandrill_inbound_emails POST   /rails/action_mailbox/mandrill/inbound_emails(.:format)                                           action_mailbox/ingresses/mandrill/inbound_emails#create
